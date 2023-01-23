b98189a6a17530f6edc2c3f5d94dc7137411515c
Sacha <40305546+SachaPass@users.noreply.github.com>
2023-01-23 12:43:43 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+import React, { Component } from "react";
+import ".CSS/stylesheet.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div id="hero"></div>
+
+        <div className="menu-container">
+          <a href="#" className="menu-item">
+            Home
+          </a>
+          <a href="#" className="menu-item">
+            Explore
+          </a>
+          <div className="menu-text"></div>
+          <a href="CreateAccount.html" className="menu-item">
+            Start a campaign
+          </a>
+          <a href="#" className="menu-item">
+            How it works
+          </a>
+          <a href="startACampaign.html" className="menu-item start-campaign">
+            Start a campaign
+          </a>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
